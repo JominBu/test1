714e249c5ed0e9e988c5e0325baa889edccc6418
buxiaoming <buxiaoming@jd.com>
2016-10-27 14:47:49 +0800

    计算任选 3个 （1 到 9 ）的自然数，他们能通过 加减乘数 运算组合 形成 24 。例如 ， 1，3，8 就能 通过 1X3X8 这样的运算 得到 24.   或者 7+8 +9 = 24 把所有 符合这个条件的数字 组合 列出来。 不考虑顺序。 7，8，9 和 8，9，7 是一组数字。

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: buxiaoming
@@ -8,16 +12,45 @@
 public class Test {
 
     public static void main(String[] args) {
-        String[] a = new String[6];
-        a[0]="1";
-        a[1]="2";
-        a[2]="3";
-        String[] b = new String[3];
-        b[0]="4";
-        b[1]="5";
-        b[2]="6";
-        System.arraycopy(b, 0, a, 3, b.length);
-        System.out.println(a[3]);
+//        String[] a = new String[6];
+//        a[0]="1";
+//        a[1]="2";
+//        a[2]="3";
+//        String[] b = new String[3];
+//        b[0]="4";
+//        b[1]="5";
+//        b[2]="6";
+//        System.arraycopy(b, 0, a, 3, b.length);
+//        System.out.println(a[3]);
+
+        List<Integer> list = new ArrayList<Integer>();
+        List<String> list1 = new ArrayList<String>();
+        for(int i=1; i < 10; i++) {
+            for (int j=1; j < 10; j++){
+                for (int n=1; n < 10; n++){
+                    int a = i + j + n;
+                    int b = i * j * n;
+                    if (a == 24) {
+                        Integer[] ints = new Integer[3];
+                        ints[0] = i;
+                        ints[1] = j;
+                        ints[2] = n;
+                        Arrays.sort(ints);
+                        String s = ints[0]+""+ints[1]+""+ints[2];
+                        if (!list1.contains(s)) {
+                            list1.add(s);
+                            System.out.println("加：" + i+" "+j+" "+n);
+                        }
+                    }
+                    if (b == 24) {
+                        if (!list.contains(a)) {
+                            list.add(a);
+                            System.out.println("乘：" + i+" "+j+" "+n);
+                        }
+                    }
+                }
+            }
+        }
 
     }
 
